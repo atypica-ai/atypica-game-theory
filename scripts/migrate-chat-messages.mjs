90dc8ad7c72322e34f4254f9e40d160f47e8b3cd
web3nomad.eth <me@web3nomad.eth>
2025-04-12 22:50:03 +0800

    aemnd chat migrate script

--- scripts/migrate-chat-messages.mjs
+++ scripts/migrate-chat-messages.mjs
@@ -56,6 +56,7 @@ async function migrateInterviewChatMessages() {
         data: { interviewUserChatId: userChat.id },
       });
       let lastCreatedAt = interview.createdAt;
+      const records = [];
       for (const message of interview.messages) {
         let { id: messageId, role, content, parts, createdAt, ...extra } = message;
         if (!messageId) {
@@ -81,19 +82,18 @@ async function migrateInterviewChatMessages() {
           "extra:",
           JSON.stringify(extra).length,
         );
-        await prisma.chatMessage.create({
-          data: {
-            messageId,
-            userChatId: userChat.id,
-            role,
-            content,
-            parts,
-            extra,
-            createdAt,
-            updatedAt: createdAt,
-          },
+        records.push({
+          messageId,
+          userChatId: userChat.id,
+          role,
+          content,
+          parts,
+          extra,
+          createdAt,
+          updatedAt: createdAt,
         });
       }
+      await prisma.chatMessage.createMany({ data: records });
       await prisma.analystInterview.update({
         where: { id: interview.id },
         data: { messages: [] },
@@ -108,12 +108,11 @@ async function migrateInterviewChatMessages() {
 
 async function migrateUserChatMessages() {
   try {
-    const studyUserChats = await prisma.userChat.findMany({
+    const userChats = await prisma.userChat.findMany({
       select: { id: true },
       orderBy: { id: "asc" },
     });
-    console.log(`${studyUserChats.length} study user chats`);
-    for (const { id: chatId } of studyUserChats) {
+    for (const { id: chatId } of userChats) {
       const userChat = await prisma.userChat.findUnique({
         where: { id: chatId },
         select: { id: true, messages: true, createdAt: true },
@@ -124,8 +123,9 @@ async function migrateUserChatMessages() {
       }
       console.log("userchat", userChat.id, "migrating");
       let lastCreatedAt = userChat.createdAt;
+      const records = [];
       for (const message of userChat.messages) {
-        let { id: messageId, role, content, parts, createdAt, ...extra } = message;
+        let { id: messageId, role, content: _content, parts, createdAt, ...extra } = message;
         if (!messageId) {
           messageId = generateId();
         }
@@ -138,6 +138,10 @@ async function migrateUserChatMessages() {
         if (!parts) {
           parts = [];
         }
+        let content = _content;
+        if (userChat.id === 558) {
+          content = content.substring(0, 5000); // 这个消息有问题，太长了
+        }
         console.log(
           createdAt,
           messageId,
@@ -149,19 +153,18 @@ async function migrateUserChatMessages() {
           "extra:",
           JSON.stringify(extra).length,
         );
-        await prisma.chatMessage.create({
-          data: {
-            messageId,
-            userChatId: userChat.id,
-            role,
-            content,
-            parts,
-            extra,
-            createdAt,
-            updatedAt: createdAt,
-          },
+        records.push({
+          messageId,
+          userChatId: userChat.id,
+          role,
+          content,
+          parts,
+          extra,
+          createdAt,
+          updatedAt: createdAt,
         });
       }
+      await prisma.chatMessage.createMany({ data: records });
       await prisma.userChat.update({
         where: { id: userChat.id },
         data: { messages: [] },
@@ -174,9 +177,76 @@ async function migrateUserChatMessages() {
   }
 }
 
+async function fixEmptyParts() {
+  try {
+    const chatMessages = await prisma.chatMessage.findMany({
+      where: {
+        parts: { equals: [] },
+      },
+    });
+    console.log(`${chatMessages.length} chat messages`);
+    for (const { id, content } of chatMessages) {
+      await prisma.chatMessage.update({
+        where: { id },
+        data: {
+          parts: [{ type: "text", text: content }],
+        },
+      });
+    }
+  } catch (error) {
+    console.log("Error fix empty parts:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function fixPaymentParts() {
+  try {
+    const chatMessages = await prisma.chatMessage.findMany({
+      where: {
+        content: { contains: "免费研究额度已经用完，不如请一杯咖啡再做一份研究？" },
+      },
+    });
+    console.log(`${chatMessages.length} chat messages`);
+    for (const message of chatMessages) {
+      const parts = message.parts.filter((part) => {
+        if (part.type === "text") {
+          return !part.text.includes("免费研究额度已经用完，不如请一杯咖啡再做一份研究？");
+        } else if (part.type === "tool-invocation") {
+          return !part.toolInvocation.toolName === "requestPayment";
+        }
+      });
+      console.log(parts);
+      if (parts.length) {
+        await prisma.chatMessage.update({
+          where: { id: message.id },
+          data: {
+            parts,
+            content: message.content
+              .replace("免费研究额度已经用完，不如请一杯咖啡再做一份研究？", "")
+              .trim(),
+          },
+        });
+      } else {
+        await prisma.chatMessage.delete({
+          where: { id: message.id },
+        });
+      }
+    }
+  } catch (error) {
+    console.log("Error fix payment parts:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
 async function main() {
-  await migrateUserChatMessages();
-  await migrateInterviewChatMessages();
+  // 先 git commit reset --hard 207f8752d4c92097f4a2afaafc732bc1d1a85a1f 执行下面两个，执行前运行 npx prisma generate
+  // await migrateUserChatMessages();
+  // await migrateInterviewChatMessages();
+  // 在 git merge main 到最新分支执行下面两个，执行前运行 npx prisma generate
+  // await fixEmptyParts();
+  // await fixPaymentParts();
 }
 
 await main();
