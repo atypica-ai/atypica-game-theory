9e764ee999068972a2e710088c9d5916fd25b40b
web3nomad.eth <me@web3nomad.eth>
2025-04-15 10:30:01 +0800

    add bedrock provider

--- src/lib/llm.ts
+++ src/lib/llm.ts
@@ -1,3 +1,4 @@
+import { createAmazonBedrock } from "@ai-sdk/amazon-bedrock";
 import { createDeepSeek } from "@ai-sdk/deepseek";
 import { createOpenAI } from "@ai-sdk/openai";
 
@@ -11,3 +12,23 @@ export const deepseek = createDeepSeek({
   apiKey: process.env.SILICONFLOW_API_KEY,
   baseURL: process.env.SILICONFLOW_BASE_URL,
 });
+
+const _bedrock = createAmazonBedrock({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+export const bedrock = (model: "claude-3-7-sonnet" = "claude-3-7-sonnet") => {
+  if (model === "claude-3-7-sonnet") {
+    return _bedrock("us.anthropic.claude-3-7-sonnet-20250219-v1:0", {
+      additionalModelRequestFields: {
+        // https://docs.anthropic.com/en/docs/build-with-claude/tool-use/overview#disabling-parallel-tool-use
+        // https://docs.anthropic.com/en/docs/build-with-claude/tool-use/token-efficient-tool-use
+        anthropic_beta: ["token-efficient-tools-2025-02-19"],
+      },
+    });
+  } else {
+    return _bedrock(model);
+  }
+};
