edd93d8a639e21f74ceb32a8864c0eb6986ea7c8
web3nomad.eth <me@web3nomad.eth>
2025-04-22 10:50:02 +0800

    delete history migrations before squash

--- prisma/migrations/20250405110210_userchat_token_isnot_null/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `token` on table `UserChat` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `UserChat` MODIFY `token` VARCHAR(64) NOT NULL;
--- prisma/migrations/20250419151719_add_category_to_featured_study/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `FeaturedStudy` ADD COLUMN `category` VARCHAR(64) NULL;
