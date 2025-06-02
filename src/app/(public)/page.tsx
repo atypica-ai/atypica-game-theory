035be14a0976f0138c52c60e37d26101053a3f20
web3nomad.eth <me@web3nomad.eth>
2025-06-03 02:34:40 +0800

    new homepage

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,6 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth/next";
-import HomePageClient from "./HomePageClient";
+import HomePageClient from "./home/HomePageClient";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
