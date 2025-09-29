c575512615a7d3b34babd0010c7fa0306c56a627
web3nomad.eth <me@web3nomad.eth>
2025-09-30 03:48:57 +0800

    feat: add Google Ads tracking support

--- src/lib/analytics/config.ts
+++ src/lib/analytics/config.ts
@@ -5,6 +5,10 @@ export async function googleAnalyticsMeasurementId() {
   return process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID;
 }
 
+export async function googleAdsId() {
+  return process.env.GOOGLE_ADS_ID;
+}
+
 export async function segmentAnalyticsWriteKey() {
   return process.env.SEGMENT_ANALYTICS_WRITE_KEY;
 }
